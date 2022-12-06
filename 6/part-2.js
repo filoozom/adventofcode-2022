3570df1924f96c7fe93655d33e2b7a15efa70cb5
Philippe Schommers <philippe@schommers.be>
2022-12-06 11:58:03 +0100

    feat: add day 6

---
+++ 6/part-2.js
@@ -0,0 +1,18 @@
+import { readFile } from "node:fs/promises";
+
+const file = await readFile("./input", "utf8");
+const size = 14;
+const buffer = [];
+
+for (let i = 0; i < file.length; i++) {
+  buffer.push(file[i]);
+
+  if (buffer.length > size) {
+    buffer.shift();
+  }
+
+  if (new Set(buffer).size === size) {
+    console.log(i + 1);
+    break;
+  }
+}
